9b1c14e13db5fc856bcb84b05620ca4765678aca
A. Roman <zdzichuzdybel@gmail.com>
2023-05-26 21:23:36 +0200

    Upload Solution

--- src/main/java/efs/task/functional/PeopleProcessor.java
+++ src/main/java/efs/task/functional/PeopleProcessor.java
@@ -1,24 +1,26 @@
 package efs.task.functional;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 class PeopleProcessor {
 
     // TODO: kontynuuj implementację metody tak, aby dla kolekcji (people) obiektów klasy Person zwracała listę
     //       imion ludzi, których wiek jest większy niż podany w argumencie age
     List<String> namesOfPeopleWhoseAgeIsGreaterThan(int age, Collection<Person> people) {
-        return people.stream()
+        return people.stream().filter(person -> person.getAge() > age).map(Person::getName).collect(Collectors.toList());
     }
 
     // TODO: kontynuuj implementację metody tak, aby dla kolekcji (people) obiektów klasy Person zwracała listę
     //       imion ludzi żyjących w danym mieście (cityOfLiving). Lista ma być posortowana po wieku osób.
     List<String> namesOfPeopleSortedByAgeLivingIn(String cityOfLiving, Collection<Person> people) {
-        return people.stream()
+        return people.stream().filter(person -> person.getCityOfLiving() == cityOfLiving).sorted((Person o1, Person o2) -> Integer.compare(o1.getAge(), o2.getAge())).map(Person::getName).collect(Collectors.toList());
     }
-
     // TODO: kontynuuj implementację metody tak, aby dla kolekcji (people) obiektów klasy Person zwracała mapę
     //       z informacją o średnim wieku osób w poszczególnych miastach
     Map<String, Double> averageAgeByCityOfLiving(Collection<Person> people) {
-        return people.stream()
+        return people.stream().collect(groupingBy(Person::getCityOfLiving, averagingInt(Person::getAge)));
     }
 }
